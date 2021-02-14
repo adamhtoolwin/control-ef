a102066e66ecab24e4cb728706edbf248603d36d
FathimaShafana <77454499+FathimaShafana@users.noreply.github.com>
2021-02-14 15:26:26 +0530

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into video (title,keyword,upload,uploader) values ('Top 10 NXT','WWE','2021-02-12','Smith');
+insert into video (title,keyword,upload,uploader) values ('The Holy Travel','Travel','2019-12-12','Natasha');
+insert into video (title,keyword,upload,uploader) values ('The Beautiful','Story','2020-12-12','Shin Thant');
+insert into video (title,keyword,upload,uploader) values ('OST Tamil','Music','2020-10-10','Graham');
