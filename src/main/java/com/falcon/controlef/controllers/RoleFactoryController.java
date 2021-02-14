bb06fc65aa0f78e84add8a162b48700aa370d07d
FathimaShafana <77454499+FathimaShafana@users.noreply.github.com>
2021-02-14 17:02:24 +0530

    Add files via upload

---
+++ src/main/java/com/falcon/controlef/controllers/RoleFactoryController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.RoleFactory;
+import com.example.demo.models.UserRole;
+
+@RestController
+@RequestMapping("/")
+public class RoleFactoryController {
+	
+	private RoleFactory factory;
+	
+	@Autowired
+	public RoleFactoryController(RoleFactory factory) {
+		this.factory=factory;
+	}
+	
+	//Create a new role (type) for the given name and id 
+	@PostMapping("addNewRole/{type}/{name}/{id}")
+	public UserRole addNewRole(@PathVariable String type, @PathVariable String name, @PathVariable String id) {
+		UserRole role = this.factory.createRole(type);
+		role.setName(name);
+		role.setId(id);
+		role.checkAccess();
+		return role;
+	}
+}
+
