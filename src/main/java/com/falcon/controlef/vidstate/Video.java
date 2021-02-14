3d126fee6f58f4a55465cc04ace16159d941b7a4
Gleb <st122002@ait.asia>
2021-02-14 16:50:11 +0700

    Implement State

---
+++ src/main/java/com/falcon/controlef/vidstate/Video.java
@@ -0,0 +1,23 @@
+package com.falcon.controlef.vidstate;
+
+public class Video {
+	State state; //the class can have a state
+
+	public Video() {
+		state = new StateNull();
+	}
+
+	// if ( the video state number goes up )
+	public void stateUp() {
+		state.stateUp();
+	}
+
+	// if ( an error is encountered in the database or other processes)
+	public void errorEncountered() {
+		state.errorEncountered();
+	}
+
+	public void printState() {
+		state.printState();
+	}
+}
