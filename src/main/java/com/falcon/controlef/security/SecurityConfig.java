6f2598d0a2b590fa1304af9b0110fa74fca1e62f
FathimaShafana <77454499+FathimaShafana@users.noreply.github.com>
2021-02-21 16:53:34 +0530

    Update SecurityConfig.java

--- src/main/java/com/falcon/controlef/security/SecurityConfig.java
+++ src/main/java/com/falcon/controlef/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.security;
+package com.falcon.controlef.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
