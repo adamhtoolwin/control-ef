ac7a3e2a4e7c3d5b4bf0766da270968efb98d0d8
FathimaShafana <77454499+FathimaShafana@users.noreply.github.com>
2021-02-21 17:20:32 +0530

    Update UserValidator.java

--- src/main/java/com/falcon/controlef/validation/UserValidator.java
+++ src/main/java/com/falcon/controlef/validation/UserValidator.java
@@ -1,12 +1,12 @@
-package com.example.demo.validation;
+package com.falcon.controlef.validation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import com.example.demo.models.User;
-import com.example.demo.service.UserService;
+import com.falcon.controlef.models.User;
+import com.falcon.controlef.service.UserService;
 
 @Component
 public class UserValidator implements Validator{
