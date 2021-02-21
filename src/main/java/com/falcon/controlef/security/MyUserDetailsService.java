1a42328e1c606badbb13351bce8225454ccfcbcd
FathimaShafana <77454499+FathimaShafana@users.noreply.github.com>
2021-02-21 16:48:09 +0530

    Add files via upload

---
+++ src/main/java/com/falcon/controlef/security/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.demo.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.UserDao;
+import com.example.demo.models.User;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserDao userDao;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+		User user = userDao.findByUsername(username);
+		if(user==null)
+			throw new UsernameNotFoundException("User 404");
+		return new UserDetailsImpl(user);
+	}
+}
