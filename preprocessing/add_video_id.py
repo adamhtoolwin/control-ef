5c46562aa84956714a5cce54e7fdcd1c9f2e4134
AdamHtooLwin <adamhtoolwin@gmail.com>
2021-04-26 00:49:14 +0700

    Add another ssh script for guppy and more file preprocessing

--- preprocessing/add_video_id.py
+++ preprocessing/add_video_id.py
@@ -34,6 +34,9 @@ if __name__ == "__main__":
 
     errors = []
 
+    columns = ['start_time', 'end_time', 'content', 'video_id']
+    data_df = pd.DataFrame(columns=columns)
+
     for csv in pbar:
         pbar.set_description("Processing %s" % csv)
 
@@ -45,25 +48,44 @@ if __name__ == "__main__":
 
             video_data['id'].append(video_id)
 
-            df.drop('Number', 1, inplace=True)
+            # new_start = data_df['start_time'].append(df['Start time in milliseconds'], ignore_index=True).rename(
+            #     'start_time')
+            # new_end = data_df['end_time'].append(df['End time in milliseconds'], ignore_index=True).rename('end_time')
+            # new_content = data_df['content'].append(df['Text'], ignore_index=True).rename('content')
+            # new_video = data_df['video_id'].append(pd.Series(video_id, index=df.index), ignore_index=True).rename(
+            #     'video_id')
 
-            df.columns = ['start_time', 'end_time', 'content']
+            new_start = df['Start time in milliseconds'].rename('start_time')
+            new_end = df['End time in milliseconds'].rename('end_time')
+            new_content = df['Text'].rename('content')
+            new_video = pd.Series(video_id, index=df.index).rename('video_id')
 
-            df['video_id'] = pd.Series(video_id, index=df.index)
+            data_df = data_df.append(pd.concat([new_start, new_end, new_content, new_video], axis=1), ignore_index=True)
 
-            df.to_csv(output_folder + filename, index=False)
+            # data_df.columns = ['start_time', 'end_time', 'content', 'video_id']
+
         except pd.errors.ParserError:
             errors.append(csv)
 
     video_df = pd.DataFrame(video_data)
     video_df.to_csv(output_folder + "videos.csv", index=False)
 
-    f = open("logs.txt", "a")
+    # filtering out nans
+    data_df.dropna(inplace=True)
+
+    data_df.start_time = data_df.start_time.astype('int64')
+    data_df.end_time = data_df.end_time.astype('int64')
+
+    # data_df.start_time = pd.to_numeric(data_df.start_time, downcast='integer')
+    # data_df.end_time = pd.to_numeric(data_df.end_time, downcast='integer')
+    data_df.to_csv((output_folder + "transcripts.csv"), index=False)
+
+    # f = open("logs.txt", "a")
 
     print("Total errors: ", len(errors))
     print("Errors in these files: ")
     for error in errors:
-        f.write(error + "\n")
+        # f.write(error + "\n")
         print(error)
 
-    f.close()
+    # f.close()
