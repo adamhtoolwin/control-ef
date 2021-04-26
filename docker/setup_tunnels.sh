749b978da150fb69b7aa3df4d5bb90f6b55254c0
AdamHtooLwin <adamhtoolwin@gmail.com>
2021-04-27 00:16:04 +0700

    Commit backup SQL file

--- docker/setup_tunnels.sh
+++ docker/setup_tunnels.sh
@@ -4,4 +4,4 @@ ssh -fN -L 9042:localhost:4126 guppy
 ssh -fN -L 7199:localhost:7199 guppy
 ssh -fN -L 9160:localhost:9160 guppy
 ssh -fN -L 4127:localhost:4127 guppy
-
+ssh -fN -L 4128:localhost:4128 guppy
